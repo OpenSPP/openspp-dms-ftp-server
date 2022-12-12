9675813ee288d73697102b25f3578e01b9716af8
Rommel Terrence Juanillo <terrence@newlogic.com>
2022-12-12 10:13:00 +0800

    feat: 182015 Add test for authorizers and filesystem

---
+++ tests/test_filesystem.py
@@ -0,0 +1,21 @@
+from urllib.request import FTPHandler
+
+from filesystems import OpenSPPFS
+
+
+class TestOpenSPPFS:
+    def test_file_open(self):
+        filename = "tests/test_handlers.py"
+        fs = OpenSPPFS("/home/root/", FTPHandler)
+        opened_file = fs.open(filename, "wb")
+        assert filename != opened_file.name
+        assert filename.replace("/", "_") in opened_file.name
+
+    def test_dir_listing(self):
+        filename = "tests/test_handlers.py"
+        fs = OpenSPPFS("/home/root/", FTPHandler)
+        dirs = fs.listdir(filename)
+        assert [] == dirs
+
+        dirs2 = fs.listdir("")
+        assert [] == dirs2
