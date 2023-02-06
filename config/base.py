743284e48a89e568a480f11a542e44349c8e3873
Rommel Terrence Juanillo <terrence@newlogic.com>
2023-02-06 14:51:07 +0800

    enhancement: Rename default OpenSPP server and change max conn/IP to unlimited

--- config/base.py
+++ config/base.py
@@ -2,9 +2,7 @@ import os
 
 DEBUG = os.getenv("DEBUG", default=True)
 TLS = os.getenv("TLS", default=False)
-OPENSPP_URL = os.getenv(
-    "OPENSPP_URL", default="https://stag-openspp-iraq.newlogic-demo.com/"
-)
+OPENSPP_URL = os.getenv("OPENSPP_URL", default="http://example.org/")
 OPENSPP_DB_NAME = os.getenv("OPENSPP_DB_NAME", default="devel")
 DEFAULT_PASSIVE_PORTS = os.getenv(
     "DEFAULT_PASSIVE_PORTS", default=[3000, 3001, 3002, 3003, 3004, 3005]
@@ -21,5 +19,5 @@ FTP_WORKER_COUNT = os.getenv("FTP_WORKER_COUNT", default=1)
 FTP_UPLOAD_DIR = os.getenv("FTP_UPLOAD_DIR", default="/uploads")
 FTP_MAX_CONNECTIONS = int(os.getenv("FTP_MAX_CONNECTIONS", default=256))
 FTP_MAX_CONNECTIONS_PER_IP = int(
-    os.getenv("FTP_MAX_CONNECTIONS_PER_IP", default=10)
+    os.getenv("FTP_MAX_CONNECTIONS_PER_IP", default=0)
 )  # 0 for unlimited
